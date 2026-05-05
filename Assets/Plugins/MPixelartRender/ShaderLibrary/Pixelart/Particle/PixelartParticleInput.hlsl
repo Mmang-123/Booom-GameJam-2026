e88dd109d137ff7fa7a86fc978ade2b56d5820c3
Mmang-123 <mmang_123@163.com>
2026-05-05 11:25:09 +0800

    feat: 像素粒子

---
+++ Assets/Plugins/MPixelartRender/ShaderLibrary/Pixelart/Particle/PixelartParticleInput.hlsl
@@ -0,0 +1,13 @@
+﻿
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/DebugMipmapStreamingMacros.hlsl"
+
+struct PixelartParticleInput
+{
+    float3 positionOS : POSITION;
+    half4 color : COLOR;
+    float4 texcoord0 : TEXCOORD0;
+    float4 texcoord1 : TEXCOORD1;
+    float4 normalOS : NORMAL;
+    float4 tangentOS : TANGENT;
+};
