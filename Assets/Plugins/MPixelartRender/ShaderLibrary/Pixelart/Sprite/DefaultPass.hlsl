06435b834097e929d240d177b4f81d905f2455bb
Mmang-123 <mmang_123@163.com>
2026-05-09 10:03:12 +0800

    feat: DefaultSprite的自发光

--- Assets/Plugins/MPixelartRender/ShaderLibrary/Pixelart/Sprite/DefaultPass.hlsl
+++ Assets/Plugins/MPixelartRender/ShaderLibrary/Pixelart/Sprite/DefaultPass.hlsl
@@ -117,6 +117,8 @@ float4 UnlitFrag(Varyings input) : SV_Target
         outputColor.rgb = lerp(LightenBlend(outputColor.rgb, lightColor, _LightenBlend), outputColor.rgb * lightColor, _ShadingBlend);
     }
 
+    outputColor.rgb = lerp(outputColor.rgb, _Emission.rgb, _Emission.a);
+
     // return float4(lightColor, outputColor.a);
     return outputColor;
 }
