dd6954c72f0d3c28f2fa251d56fc41645443c3f0
Mmang-123 <mmang_123@163.com>
2026-05-12 21:26:37 +0800

    feat: 退出游戏

---
+++ Assets/Code/Game/Scripts/Mechanics/ExitGameMachine.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class ExitGameMachine : MonoBehaviour, IChargable
+    {
+        #region IChargable
+        public PowerSourceHandler PowerSourceHandler { get; } = new();
+        public bool IsPowered => PowerSourceHandler.IsPowered();
+
+        #endregion
+
+        private void Update()
+        {
+            if (IsPowered)
+                ExitGame();
+        }
+
+        private void ExitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        public void SetChargeComplete(bool init)
+        {
+            
+        }
+    }
+}
