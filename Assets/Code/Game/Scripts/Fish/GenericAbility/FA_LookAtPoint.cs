ba07c3ef49c4bbbcc630e723a17b0ffc0d37a2a3
Mmang-123 <mmang_123@163.com>
2026-05-13 20:34:27 +0800

    feat: AI看向指定点的功能

---
+++ Assets/Code/Game/Scripts/Fish/GenericAbility/FA_LookAtPoint.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+namespace Game
+{
+    public class FA_LookAtPoint : FishAIAbility
+    {
+        [SerializeField] private Transform m_TargetPoint;
+
+        private FB_Swim m_SwimBehaviour;
+
+        public override bool CanActivateAbility()
+        {
+            return true;
+        }
+
+        public override void OnActivate()
+        {
+            base.OnActivate();
+            m_SwimBehaviour = Fish.GetBehaviour<FB_Swim>();
+        }
+
+        public override void OnUpdate(float dt)
+        {
+            base.OnUpdate(dt);
+            m_SwimBehaviour.RotateToTargetPoint = true;
+            m_SwimBehaviour.Tracing = false;
+            m_SwimBehaviour.TargetPoint = m_TargetPoint.position;
+        }
+    }
+}
