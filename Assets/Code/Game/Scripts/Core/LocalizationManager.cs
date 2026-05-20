2745fd899a9380b246eeade57fbf2fb5e09722c4
Mmang-123 <mmang_123@163.com>
2026-05-20 21:51:22 +0800

    feat: 识别系统语言

--- Assets/Code/Game/Scripts/Core/LocalizationManager.cs
+++ Assets/Code/Game/Scripts/Core/LocalizationManager.cs
@@ -1,14 +1,21 @@
 ﻿
 using Mmang.Util;
+using UnityEngine;
 
 namespace Game
 {
     public class LocalizationManager : SingletonMono<LocalizationManager>
     {
-        public enum ELanguage { Chinese, English }
+        public enum ELanguage { Chinese = 0, English = 1 }
 
         public static ELanguage CurrentLanguage { get; private set; }
         public static System.Action<ELanguage> OnLanguageChanged;
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+            InitLanguage();
+        }
 
         public void SetLanguage(ELanguage language)
         {
@@ -16,6 +23,24 @@ namespace Game
                 return;
             CurrentLanguage = language;
             OnLanguageChanged?.Invoke(language);
+            PlayerPrefs.SetInt("Language", (int)CurrentLanguage);
+        }
+
+        private void InitLanguage()
+        {
+            if (PlayerPrefs.HasKey("Language"))
+            {
+                int val = PlayerPrefs.GetInt("Language");
+                CurrentLanguage = (ELanguage)val;
+                return;
+            }
+
+            var systemLang = Application.systemLanguage;
+            if (systemLang == SystemLanguage.Chinese || systemLang == SystemLanguage.ChineseSimplified || systemLang == SystemLanguage.ChineseTraditional)
+                CurrentLanguage = ELanguage.Chinese;
+            else
+                CurrentLanguage = ELanguage.English;
+            PlayerPrefs.SetInt("Language", (int)CurrentLanguage);
         }
     }
 }
