e01fe9a0842a8456a4a0a80d018adc0ce42ffa05
Mmang-123 <mmang_123@163.com>
2026-05-20 18:13:28 +0800

    feat: 动态UI

---
+++ Assets/Code/Game/Scripts/UI/DynamicUI.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+
+namespace Game
+{
+    public class DynamicUI : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer m_Renderer;
+        [SerializeField] private Sprite m_MouseSprite;
+        [SerializeField] private Sprite m_KeyboardSprite;
+        [SerializeField] private Sprite m_GamepadSprite;
+
+        private void Start()
+        {
+            OnDeviceChanged(GameInputManager.CurrentControlScheme);
+        }
+        
+        private void OnEnable()
+        {
+            GameInputManager.OnInputDeviceChanged += OnDeviceChanged;
+        }
+
+        private void OnDisable()
+        {
+            if (GameInputManager.InstanceValid)
+                GameInputManager.OnInputDeviceChanged -= OnDeviceChanged;
+        }
+
+        public void OnDeviceChanged(GameInputManager.EControlMode controlMode)
+        {
+            switch (controlMode)
+            {
+                case GameInputManager.EControlMode.Mouse:
+                    m_Renderer.sprite = m_MouseSprite;
+                    break;
+                case GameInputManager.EControlMode.Keyboard:
+                    m_Renderer.sprite = m_KeyboardSprite;
+                    break;
+                case GameInputManager.EControlMode.Gamepad:
+                    m_Renderer.sprite = m_GamepadSprite;
+                    break;
+            }
+        }
+    }
+}
